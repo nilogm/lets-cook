465af0b923569a44384d442a8137e44a9f429348
hulloCode <nilogm2012@gmail.com>
2023-06-01 17:01:01 -0300

    Updating screen components

--- app/src/pages/Search/index.js
+++ app/src/pages/Search/index.js
@@ -1,10 +1,41 @@
-import { View } from "react-native";
-import RecipeSearch from "../../components/RecipeSearch";
+import { FlatList, View } from "react-native";
+import RecipeSearchButton from "../../components/RecipeSearchButton"
 
-export default function Search({ navigation }) {
+const Spacing = () => (
+    <View style={{ height: 20, width: "100%" }} />
+);
+
+export default function RecipeSearch({ navigation }) {
+
+    const DATA = [
+        {
+            title: "Frango Grelhado",
+            image: "https://spoonacular.com/recipeImages/716429-312x231.jpg"
+        },
+        {
+            title: "Milkshake de Baunilha",
+            image: "https://spoonacular.com/recipeImages/715538-312x231.jpg"
+        },
+        {
+            title: "Salada Ceasar",
+            image: "https://spoonacular.com/recipeImages/73420-312x231.jpg"
+        },
+        {
+            title: "Biscoito de Aveia",
+            image: "https://spoonacular.com/recipeImages/73420-312x231.jpg"
+        },
+    ]
+
     return (
         <View>
-            <RecipeSearch navigation={navigation} />
+            <FlatList
+                vertical={true}
+                scrollEnabled={true}
+                data={DATA}
+                contentContainerStyle={{ alignItems: "center" }}
+                renderItem={({ item }) => (<RecipeSearchButton navigation={navigation} title={item.title} image={item.image} />)}
+                ItemSeparatorComponent={Spacing()}
+            />
         </View>
-    );
+    )
 }
