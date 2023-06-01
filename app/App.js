19dbd8b1810b1f0be7c9201281d3011796f28bfe
gui751 <guisgb10@gmail.com>
2023-06-01 16:51:41 -0300

    Starting reading API

--- app/App.js
+++ app/App.js
@@ -9,7 +9,42 @@ import Recipe from "./src/pages/Recipe"
 
 const Stack = createNativeStackNavigator();
 
+const [data, setData] = useState([]);
+
+const getRecipes = async (ingredients, macros) => {
+  try {
+    var instrucoes;
+    
+    instrucoes = {
+      "modoPreparo" : [
+      ]
+    }
+    const numberofRecipes = 2;
+    const response = await fetch('https://api.spoonacular.com/recipes/complexSearch?apiKey=ed5efa73e002400393a5034f3327b3c4&includeIngredients=' + ingredients +'&' 
+                                + macros + '&number='+numberofRecipes);
+    const json = await response.json();
+    const recipes = json.results;
+    setData(recipes);
+  
+  
+} catch (error) {
+  console.error(error);
+} finally {
+  setLoading(false);
+}
+};
+
+
 function App() {
+
+  ingredients = "tomato,cheese";
+  macros = "maxFat=25&maxCholesterol=100";
+
+  useEffect(() => {
+    getRecipes(ingredients, macros);
+  }, []);
+  recipes = data;
+  console.log(recipes);
   return (
     <NavigationContainer>
       <Stack.Navigator>
